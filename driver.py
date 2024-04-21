a0e45efcac5c7ff565e7454ce05d8407bdafe74e
Chris Durbin <chrisatumd@gmail.com>
2024-04-21 16:27:50 -0400

    Add commandline options for running.

--- driver.py
+++ driver.py
@@ -3,23 +3,17 @@ import q_learning_agent
 import blackjack_env
 import dqn_agent
 import bet_size_dqn_agent
+import random_agent
 
 import numpy as np
 import os
 import sys
+import argparse
 
 BET_SIZE = {0: 10, 1: 500}
 
-# Usage: `python driver.py` to run the assignment as specified
-# To run a different test scenario (described below) run `python driver.py <scenario number>`
-def main(scenario=1, agent_type=1):
-    print(f'Running scenario {scenario}')
-    # NUM_EPISODES = 2000
-    # NUM_AGENTS = 10
-    # NUM_EPISODES = 100000000
-    NUM_EPISODES = 100000
-    # NUM_EPISODES = 4000
-    NUM_AGENTS = 1
+def main(scenario, agent_type, num_episodes, num_agents, use_bet_sizes):
+    print(f'Running scenario {scenario} with {num_agents} {agent_type} agents and {num_episodes} episodes')
     agents_win_rates = []
     agents_win_rates_excluding_ties = []
     agents_cumulative_rewards = []
@@ -31,32 +25,39 @@ def main(scenario=1, agent_type=1):
         print('Using Q-learning agent')
     else:
         print('Using DQN agent')
-    for a in range(NUM_AGENTS):
+    for a in range(num_agents):
         agents_win_rates.append([])
         agents_win_rates_excluding_ties.append([])
         agents_cumulative_rewards.append([])
         agents_balance.append([])
         agents_bet_sizes.append([])
         environment = blackjack_env.Blackjack()
-        if agent_type == 1:
+        if agent_type == 'monte-carlo':
             agent = monte_carlo_agent.RlAgent()
-        elif agent_type == 2:
+        elif agent_type == 'q-learning':
             agent = q_learning_agent.QLearningAgent()
-        else:
+        elif agent_type == 'dqn':
             agent = dqn_agent.DQNAgent()
+        elif agent_type == 'random':
+            agent = random_agent.RandomAgent()
+        else:
+            print(f'Unknown agent type {agent_type}')
+            exit(1)
         bet_agent = bet_size_dqn_agent.BetSizeDQNAgent()
         wins = 0
         ties = 0
         cumulative_rewards = 0
         agent_balance = 0
-        for i in range(NUM_EPISODES):
+        current_bet = 0
+        for i in range(num_episodes):
             if (i % 1000 == 0):
                 print(f'Starting episode {i + 1}')
             # reset the game and observe the current state
             deck_state = environment.reset()
 
             # TODO take the deck state and pass it in to the bet agent to get the bet
-            current_bet = bet_agent.select_action(deck_state)
+            if use_bet_sizes:
+                current_bet = bet_agent.select_action(deck_state)
 
             # If current_bet is 0 I expect to lose and if current_bet is 1 I expect to win
             current_state = environment.deal_hand()
@@ -97,28 +98,31 @@ def main(scenario=1, agent_type=1):
             # if current_bet == 0:
             #     opposite_bet = 1
 
-            bet_agent.learn(deck_state, current_bet, deck_state, bet_reward, True)
+            if use_bet_sizes:
+                bet_agent.learn(deck_state, current_bet, deck_state, bet_reward, True)
             # bet_agent.learn(deck_state, opposite_bet, deck_state, -bet_reward, True)
 
             if scenario == 1:
                 agents_win_rates[a].append(wins / (i + 1.0))
                 agents_win_rates_excluding_ties[a].append(wins / max([1, (i + 1.0 - ties)]))
                 agents_cumulative_rewards[a].append(cumulative_rewards)
-                agents_balance[a].append(agent_balance)
-                agents_bet_sizes[a].append(BET_SIZE[current_bet])
+                if use_bet_sizes:
+                    agents_balance[a].append(agent_balance)
+                    agents_bet_sizes[a].append(BET_SIZE[current_bet])
 
 
         if scenario == 1:
-            print(f"Agent {a} Win rate: {wins / (NUM_EPISODES):.2f}, Win rate excluding ties: {wins / (NUM_EPISODES - ties):.2f}")
-            print(f"Agent {a} Wins: {wins}, losses: {(NUM_EPISODES - ties - wins)}, ties: {ties}")
-            print(f'Agent {a} Final balance: {agent_balance}')
+            print(f"Agent {a} Win rate: {wins / (num_episodes):.2f}, Win rate excluding ties: {wins / (num_episodes - ties):.2f}")
+            print(f"Agent {a} Wins: {wins}, losses: {(num_episodes - ties - wins)}, ties: {ties}")
+            if use_bet_sizes:
+                print(f'Agent {a} Final balance: {agent_balance}')
 
         # Exploit only
         if scenario == 2:
             wins = 0
             ties = 0
             agent.epsilon = 0.0
-            for i in range(NUM_EPISODES):
+            for i in range(num_episodes):
                 current_state = environment.reset()
                 game_end = False
                 while not game_end:
@@ -134,9 +138,10 @@ def main(scenario=1, agent_type=1):
                         agents_win_rates[a].append(wins / (i + 1.0))
                         agents_win_rates_excluding_ties[a].append(wins / max([1, (i + 1.0 - ties)]))
                         agents_cumulative_rewards[a].append(cumulative_rewards)
-            print(f"Agent {a} win rate while exploiting and excluding ties: {wins / (NUM_EPISODES - ties):.2f}")
-            print(f"Agent {a} wins: {wins}, losses: {(NUM_EPISODES - ties - wins)}, ties: {ties}\n")
-            print(f'Agent {a} Final balance: {agents_balance}')
+            print(f"Agent {a} win rate while exploiting and excluding ties: {wins / (num_episodes - ties):.2f}")
+            print(f"Agent {a} wins: {wins}, losses: {(num_episodes - ties - wins)}, ties: {ties}\n")
+            if use_bet_sizes:
+                print(f'Agent {a} Final balance: {agents_balance}')
 
     if scenario == 1 or scenario == 2:
         avg_win_rate = np.mean(agents_win_rates, axis=0)
@@ -151,21 +156,44 @@ def main(scenario=1, agent_type=1):
     np.save('output/agents_win_rates', agents_win_rates)
     np.save('output/agents_cumulative_rewards', agents_cumulative_rewards)
     np.save('output/agents_balance', agents_balance)
-    np.save('output/agents_bet_sizes', agents_bet_sizes)
+    if use_bet_sizes:
+        np.save('output/agents_bet_sizes', agents_bet_sizes)
 
     print("Program completed successfully.")
 
-if __name__ == "__main__":
-    scenario = 1
-    if len(sys.argv) >= 2:
-        try:
-            scenario = int(sys.argv[1])
-            if scenario != 1 and scenario != 2:
-                print(f"The passed in scenario number {scenario} is invalid, it must be 1 or 2.")
-                exit(1)
-        except:
-            print("The passed in scenario must be an integer: 1 or 2.")
-            exit(1)
+def positive_int(value):
+    ivalue = int(value)
+    if ivalue <= 0:
+        raise argparse.ArgumentTypeError(f"{value} is an invalid positive int value")
+    return ivalue
 
-    agent_type = 2
-    main(scenario, agent_type)
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='Runs the blackjack simulation')
+
+    parser.add_argument('-t', '--agent-type',
+                        choices=['random', 'q-learning','dqn', 'monte-carlo'],
+                        default='dqn',
+                        help='Type of RL agent (random, q-learning, dqn, or monte-carlo).')
+
+    parser.add_argument('-e', '--num-episodes',
+                        type=positive_int,
+                        default=100,
+                        help='Number of episodes (positive integer).')
+
+    parser.add_argument('-a', '--num-agents',
+                        type=positive_int,
+                        default=1,
+                        help='Number of agents (positive integer).')
+
+    parser.add_argument('-s', '--scenario',
+                    type=positive_int,
+                    default=1,
+                    help='Number of agents (positive integer).')
+
+    parser.add_argument('-b', '--bet-sizes',
+                type=bool,
+                default=False,
+                help='Whether to attempt to learn bet sizes.')
+
+    args = parser.parse_args()
+    main(args.scenario, args.agent_type, args.num_episodes, args.num_agents, args.bet_sizes)
